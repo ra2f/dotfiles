aa4267615db25300ce4805093b6ae83fbb5868d9
ra2f <ra2f>
2024-02-13 22:52:25 +0100

    [*] Change owner of files

--- install/install.sh
+++ install/install.sh
@@ -7,23 +7,29 @@ dotfiles=$HOME/dotfiles
 
 msg "Installing $HOME/.config..."
 cp -TR $dotfiles/.config $HOME/.config
+chown -R "$SUDO_USER":"$SUDO_USER" $HOME/.config
 msg "Setting up Neovim ..."
 nvim --headless +PlugInstall +qa
 
 msg "Installing $HOME/.local..."
 cp -TR $dotfiles/.local $HOME/.local
+chown -R "$SUDO_USER":"$SUDO_USER" $HOME/.local
 
 msg "Updating $HOME/.bash_login..."
 cp -f $dotfiles/.bash_login $HOME/.bash_login
+chown "$SUDO_USER":"$SUDO_USER" $HOME/.bash_login
 
 msg "Updating $HOME/.bash_logout..."
 cp -f $dotfiles/.bash_logout $HOME/.bash_logout
+chown "$SUDO_USER":"$SUDO_USER" $HOME/.bash_logout
 
 msg "Updating $HOME/.bashrc..."
 cp -f $dotfiles/.bashrc $HOME/.bashrc
+chown "$SUDO_USER":"$SUDO_USER" $HOME/.bashrc
 
 msg "Updating $HOME/.hushlogin..."
 cp -f $dotfiles/.hushlogin $HOME/.hushlogin
+chown "$SUDO_USER":"$SUDO_USER" $HOME/.hushlogin
 
 # Source config files
 msg "Sourcing config files"
