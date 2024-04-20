dcbf5b384a574409a956a4677361c8728dd07ed1
New Author Name <first.last@example.com>
2024-04-20 08:47:08 +0200

    [~] Refactor

--- .config/nvim/lua/core/plugins/lsp/settings/init.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.lua = require("core.plugins.lsp.settings.lua_ls")
-M.json = require("core.plugins.lsp.settings.jsonls")
-M.yaml = require("core.plugins.lsp.settings.yaml")
-
-return M
